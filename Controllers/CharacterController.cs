a3bd993444411bbcec23a6a3fef14e32483d1fb5
George Higbie <gehigbie@gmail.com>
2022-10-11 20:03:50 -0400

    Add post method

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -11,17 +11,47 @@ namespace first_project.Controllers
     [Route("api/[controller]")]
     public class CharacterController: ControllerBase
     {
+
+        private static Character defaultCharacter = new Character();
         private static List<Character> characters = new List<Character>{
             new Character(),
-            new Character { Name = "Sam"},
-            new Character { Name = "Strider" },
-            new Character { Name = "Gandolf", Class = RpgClass.Wizzard },
-            new Character { Name =  "Healer", Class = RpgClass.Cleric },
+            new Character { Id = 1, Name = "Sam"},
+            new Character { Id = 2, Name = "Strider" },
+            new Character { Id = 3, Name = "Gandolf", Class = RpgClass.Wizzard },
+            new Character { Id = 4, Name =  "Healer", Class = RpgClass.Cleric },
+            new Character { Id = 5, Name = "Gemli", Class = RpgClass.Knight },
         };
 
-        [HttpGet]
-        public ActionResult<List<Character>> Get()
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> GetAll()
         {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Character> Get(int id)
+        {
+            if(id < characters.Count)
+            {
+                return Ok(characters.FirstOrDefault(character => character.Id == id));
+            }
+            else
+            {
+                return Ok("No characters exists with this Id");
+            }
+        }
+
+        [HttpGet("GetDefault")]
+        public ActionResult<Character> GetDefault()
+        {
+            return Ok(defaultCharacter);
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
             return Ok(characters);
         }
     }
